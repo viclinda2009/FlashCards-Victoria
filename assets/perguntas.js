f98a56909d296e20f71c41e1e1c33de3a9b41ac4
viclinda2009 <victoria.rocha.silva@escola.pr.gov.br>
2025-06-05 15:17:29 +0000

    modificação do css e finalização das perguntas

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,23 +1,53 @@
 criaCartao(
     'Geografia',
-    'Qual a capital do Brasil?',
-    'A capital do Brasil é Brasília'
-)
-
-criaCartao(
-    'Geografia',
     'Qual a capital da França?',
-    'A capital da França é Paris'
+    'A capital da França é Paris '
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Famosos',
+    'Quantos filhos a famosa Virgínia Fonseca tem?',
+    'A famosa tem três filhos sendo um menino e duas meninas'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se escreve azul em ingles?',
+    'Blue'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual é o esporte mais famoso no Brasil?',
+    'Esse esporte se chama futebol'
+)
+
+criaCartao(
+    'Artes',
+    'Quais são as cores primas?',
+    'São elas as cores azul, amarelo e vermelho'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual é o planeta mais proximo do sol?',
+    'ele se chama Mércurio'
+)
+
+criaCartao(
+    'Biologia',
+    'O que os pandas comem?',
+    'Noemalmente eles comem bambu'
+)
+
+criaCartao(
+    'Ciência',
+    'Quantos ossos tem no corpo humano?',
+    'Há 206 ossos no corpo humano'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a obra Monalisa?',
+    'Leonardo da Vinci'
 )
